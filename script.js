9dd38f01366e43d981893f3d604beca814fdcbfc
CRSorin <rsssorinc@gmail.com>
2022-07-23 09:06:34 +0300

    edited sticky nav 2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ yearEl.textContent = currentYear;
 
 // Mobile navigation
 const btnNav = document.querySelector(".btn-mobile-nav");
-const mainHeader = document.querySelector(".main");
+const mainHeader = document.querySelector(".main--heading");
 
 btnNav.addEventListener("click", function () {
   mainHeader.classList.toggle("nav-open");
@@ -13,22 +13,31 @@ btnNav.addEventListener("click", function () {
 
 // Sticky navigation
 const featuredEl = document.querySelector(".featured--works");
+const navItems = document.querySelectorAll(".main--nav__item");
 
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     if (ent.isIntersecting) {
       console.log(ent);
+      for (let i = 1; i < navItems.length; i++) {
+        navItems[i].classList.add("hidden");
+      }
+      mainHeader.classList.add("hidden");
       document.body.classList.add("sticky");
     }
     if (!ent.isIntersecting) {
+      for (let i = 1; i < navItems.length; i++) {
+        navItems[i].classList.remove("hidden");
+      }
+      mainHeader.classList.remove("hidden");
       document.body.classList.remove("sticky");
     }
   },
   {
     root: null,
     threshold: 0,
-    rootMargin: "-200px",
+    rootMargin: "-100px",
   }
 );
 
