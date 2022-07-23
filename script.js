f8389f19f702b7be688221910a5e8c7e39a801b3
CRSorin <rsssorinc@gmail.com>
2022-07-23 14:26:25 +0300

    added enable / disable navigation when nav is open on mobile

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
+///////////////////////////////////
 // Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+///////////////////////////////////
 // Mobile navigation
 const btnNav = document.querySelector(".btn-mobile-nav");
 const mainHeader = document.querySelector(".main--heading");
 const main = document.querySelector(".main");
 
+function preventDefault(e) {
+  e.preventDefault();
+}
+
 btnNav.addEventListener("click", function () {
   main.classList.toggle("nav-open");
+  // Disable navigation
+  if (main.classList.contains("nav-open")) {
+    console.log("NAV OPEN!");
+    window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+  }
+  // Enable navigation
+  if (!main.classList.contains("nav-open")) {
+    console.log("NAV CLOSED!!");
+    window.removeEventListener("DOMMouseScroll", preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener("touchmove", preventDefault, wheelOpt);
+  }
 });
+
+var supportsPassive = false;
+try {
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      },
+    })
+  );
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent =
+  "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
 
 // Sticky navigation
 
