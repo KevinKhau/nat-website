3215308fb3b2bcd2a38cddf3d2d8583d8d4d2953
CRSorin <rsssorinc@gmail.com>
2022-07-26 12:17:51 +0300

    added guard clause if browser doesn't support ResizeObserver

--- script.js
+++ script.js
@@ -103,38 +103,41 @@ if (aboutDescEl) {
 
 ///////////////////////////////////
 // About page - timeline layout change on resize
+const timelineElements = document.querySelectorAll(".timeline");
 const timelineDesktopEl = document.querySelector(".timeline--desktop");
 const timelineMobileEl = document.querySelector(".timeline--mobile");
 const aboutSectionEl = document.querySelector(".about");
 
-// if (timelineElements.length > 0) {
-//   if (window.innerWidth <= 550) {
-//     timelineDesktopEl.classList.add("hidden-nav");
-//     timelineMobileEl.classList.remove("hidden-nav");
-//   }
-//   if (window.innerWidth > 550) {
-//     timelineMobileEl.classList.add("hidden-nav");
-//     timelineDesktopEl.classList.remove("hidden-nav");
-//   }
-// }
-// window.addEventListener("resize", (e) => console.log(e.target.innerWidth));
-
-const resizeObs = new ResizeObserver((entries) => {
-  for (let entry of entries) {
-    // console.log(entry);
-    if (entry) {
-      if (window.innerWidth <= 550) {
-        // console.log("nav-small!");
-        timelineMobileEl.classList.remove("hidden-nav");
-        timelineDesktopEl.classList.add("hidden-nav");
-      }
-      if (window.innerWidth > 550) {
-        // console.log("nav-large!");
-        timelineDesktopEl.classList.remove("hidden-nav");
-        timelineMobileEl.classList.add("hidden-nav");
+if (ResizeObserver) {
+  const resizeObs = new ResizeObserver((entries) => {
+    for (let entry of entries) {
+      // console.log(entry);
+      if (entry) {
+        if (window.innerWidth <= 550) {
+          // console.log("nav-small!");
+          timelineMobileEl.classList.remove("hidden-nav");
+          timelineDesktopEl.classList.add("hidden-nav");
+        }
+        if (window.innerWidth > 550) {
+          // console.log("nav-large!");
+          timelineDesktopEl.classList.remove("hidden-nav");
+          timelineMobileEl.classList.add("hidden-nav");
+        }
       }
     }
-  }
-});
+  });
 
-resizeObs.observe(aboutSectionEl);
+  resizeObs.observe(aboutSectionEl);
+} else {
+  if (timelineElements.length > 0) {
+    if (window.innerWidth <= 550) {
+      timelineDesktopEl.classList.add("hidden-nav");
+      timelineMobileEl.classList.remove("hidden-nav");
+    }
+    if (window.innerWidth > 550) {
+      timelineMobileEl.classList.add("hidden-nav");
+      timelineDesktopEl.classList.remove("hidden-nav");
+    }
+  }
+  window.addEventListener("resize", (e) => console.log(e.target.innerWidth));
+}
