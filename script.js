118a3d0b3176777a50d08dd40105d7ad4baa2b11
CRSorin <rsssorinc@gmail.com>
2022-08-24 07:47:05 +0300

    added lazy loading feature on the index page

--- script.js
+++ script.js
@@ -239,3 +239,40 @@ if (tabsContainer) {
     }
   });
 }
+
+// Lazy loading images
+
+// const loadImg = function (entries, observer) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) return;
+//     entry.target.src = entry.target.dataset.src;
+//     entry.target.addEventListener("load", function () {
+//       entry.target.classList.remove("lazy-img");
+//     });
+//   });
+// };
+
+const imgTargets = document.querySelectorAll("img[data-src]");
+console.log(imgTargets);
+
+const body = document.querySelector("body");
+
+const loadImg = function (entries, observer) {
+  // const [entry] = entries;
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    entry.target.src = entry.target.dataset.src;
+
+    // Replace src with data-src
+    entry.target.addEventListener("load", function () {
+      entry.target.classList.remove("lazy-loading");
+    });
+  });
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
