f93f155410ef31b4660d1c928588aaf6dda0b9b7
CRSorin <rsssorinc@gmail.com>
2022-10-18 18:34:30 +0300

    remove comments, remove dead code

--- script.js
+++ script.js
@@ -21,29 +21,6 @@ mainNav.addEventListener("click", function () {
 });
 
 ///////////////////////////////////
-// Smooth scrolling animation
-
-const allNavElements = document.querySelectorAll(".main--nav__item a:link");
-allNavElements.forEach(function (navEl) {
-  navEl.addEventListener("click", function (e) {
-    // e.preventDefault();
-    const href = navEl.getAttribute("href");
-    // console.log(href);
-    if (href === "#home") window.scrollTo({ top: 0, behavior: "smooth" });
-
-    // Scrolling for other elements
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-
-    // Close mobile navigation
-    // if (navEl.classList.contains("main--nav__item"))
-    main.classList.toggle("nav-open");
-  });
-});
-
-///////////////////////////////////
 // Sticky back-on-top button
 const btnTop = document.querySelector(".btn--top");
 
@@ -68,81 +45,8 @@ const obsBtn = new IntersectionObserver(
 obsBtn.observe(mainHeader);
 
 ///////////////////////////////////
-// Sticky navigation
-
-// const featuredLastEl = document.querySelector(
-//   ".featured--works__item:last-child"
-// );
-
-// // console.log(featuredLastEl);
-// const featuredEl = document.querySelector(".featured--works");
-// const navItems = document.querySelectorAll(".main--nav__item");
-// const containerEl = document.querySelector(".container");
-
-// const obs = new IntersectionObserver(
-//   function (entries) {
-//     const ent = entries[0];
-//     // console.log(ent);
-//     if (document.title.includes("|")) return;
-//     if (ent.isIntersecting) {
-//       if (containerEl.clientWidth >= "900") {
-//         mainHeader.classList.add("hidden");
-//         for (let i = 1; i < navItems.length; i++) {
-//           navItems[i].classList.add("hidden-nav");
-//         }
-//         document.body.classList.add("sticky");
-//       }
-//       if (containerEl.clientWidth < "900") {
-//         btnNav.classList.add("sticky");
-//       }
-//     }
-//     if (!ent.isIntersecting) {
-//       if (containerEl.clientWidth >= "900") {
-//         for (let i = 1; i < navItems.length; i++) {
-//           navItems[i].classList.remove("hidden-nav");
-//         }
-//         mainHeader.classList.remove("hidden");
-//         document.body.classList.remove("sticky");
-//       }
-//       if (containerEl.clientWidth < "900") {
-//         btnNav.classList.remove("sticky");
-//       }
-//     }
-//   },
-//   {
-//     root: null,
-//     threshold: 0,
-//     rootMargin: "480px",
-//   }
-// );
-
-// // Fix for resize after scrolling down on home page
-// const resizeFeaturedEl = new ResizeObserver((entries) => {
-//   for (let entry of entries) {
-//     if (entry) {
-//       if (window.innerWidth <= 944) {
-//         mainHeader.classList.remove("hidden");
-//         // fix resize bug??
-//         document.body.classList.remove("sticky");
-//         for (let i = 1; i < navItems.length; i++) {
-//           navItems[i].classList.remove("hidden-nav");
-//         }
-//       }
-//     }
-//   }
-// });
-
-// if (featuredEl && featuredLastEl) {
-//   obs.observe(featuredLastEl);
-//   resizeFeaturedEl.observe(featuredEl);
-// }
-// if (featuredEl) {
-//   obs.observe(featuredEl);
-//   resizeFeaturedEl.observe(featuredEl);
-// }
-
-///////////////////////////////////
 // Cancel pointer events for nav item poiting to current page
+const allNavElements = document.querySelectorAll(".main--nav__item a:link");
 allNavElements.forEach(function (navEl) {
   if (document.title.includes(navEl.textContent)) {
     navEl.classList.add("no-pointer-events", "color");
@@ -170,22 +74,16 @@ if (aboutSectionEl) {
   if (ResizeObserver) {
     const resizeObs = new ResizeObserver((entries) => {
       for (let entry of entries) {
-        // console.log(entry);
         if (entry) {
           if (window.innerWidth <= 550) {
-            // console.log("nav-small!");
             timelineMobileEl.classList.remove("hidden-nav");
             timelineDesktopEl.classList.add("hidden-nav");
           }
           if (window.innerWidth > 550) {
-            // console.log("nav-large!");
             timelineDesktopEl.classList.remove("hidden-nav");
             timelineMobileEl.classList.add("hidden-nav");
           }
         }
-        window.addEventListener("resize", (e) =>
-          console.log(e.target.innerWidth)
-        );
       }
     });
 
@@ -206,11 +104,10 @@ if (aboutSectionEl) {
 }
 
 ///////////////////////////////////
-// Canesten page - layout change for mobile view
+// Canesten & Social Pages layout change for mobile view
 const worksImages = document.querySelectorAll(".works--images__toggle");
 const worksImagesDesktop = document.querySelector(".works--img__desktop");
 const worksImagesMobile = document.querySelector(".works--img__mobile");
-// console.log(worksImages);
 if (
   document.title.includes("canesten digital") ||
   document.title.includes("social")
@@ -218,7 +115,6 @@ if (
   if (ResizeObserver) {
     const resizeObs = new ResizeObserver((entries) =>
       entries.forEach((e) => {
-        // console.log(e.contentRect.width);
         if (e.contentRect.width <= 600) {
           worksImagesDesktop.classList.add("hidden-images");
           worksImagesMobile.classList.remove("hidden-images");
@@ -245,13 +141,6 @@ if (
 }
 
 ///////////////////////////////////
-///////////////////////////////////
-///////////////////////////////////
-// Social page - layout change for mobile view
-
-///////////////////////////////////
-///////////////////////////////////
-///////////////////////////////////
 // Featured works section event delegation
 const featuredWorksItems = document.querySelectorAll(".featured--works__item");
 
@@ -263,15 +152,12 @@ function clickInfo(event) {
 
 // Use event delegation to trigger the click event on the <a> element when clicking on the <figure> container
 featuredWorksItems.forEach(function (work) {
-  // const elClass = work.classList[1];
   work.addEventListener("click", clickInfo);
 });
 
 ////////////////////////////////
 // Portfolio section event delegation
 const portfolioItems = document.querySelectorAll(".portfolio__item");
-// console.log(portfolioItems);
-
 function clickItem(event) {
   const aTagPortfolioEl = event.target.closest("h3").children[0];
   console.log(aTagPortfolioEl);
@@ -284,12 +170,9 @@ portfolioItems.forEach(function (item) {
 
 ////////////////////////////////
 // Tabbed component for the portfolio / works page
-
 const tabs = document.querySelectorAll(".portfolio__tab");
 const tabsContainer = document.querySelector(".portfolio__tab--container");
 const tabsContent = document.querySelectorAll(".portfolio__content");
-// if (tabs.length > 0) console.log(tabs);
-// if (tabsContainer) console.log(tabsContainer);
 
 // Use event delegation to select each tab (btn) instead of adding an event listener for each tab
 if (tabsContainer) {
@@ -317,15 +200,10 @@ if (tabsContainer) {
 }
 
 // Lazy loading images
-
 const imgTargets = document.querySelectorAll("img[data-src]");
-// console.log(imgTargets);
-
 const body = document.querySelector("body");
 
 const loadImg = function (entries, observer) {
-  // const [entry] = entries;
-  // entry === every single item on a horizontal line that is intersecting
   entries.forEach((entry) => {
     if (!entry.isIntersecting) return;
     entry.target.src = entry.target.dataset.src;
